cec5d6e1dfe66a33fc8211e85ff2d7c0e578f9f1
torte <torte478@gmail.com>
2016-03-17 21:35:29 +0300

    add: class Card

--- Hanobi_simple/Program.cs
+++ Hanobi_simple/Program.cs
@@ -4,12 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hanobi_simple
+namespace HanobiGame
 {
     class Program
     {
+        enum Attribute { color, rank };
+        const String Colors = "RGBYW";
+
+        class Card
+        {
+            int[] value;
+            bool checkColor;
+            bool checkRank;
+
+            public Card(int color, int rank)
+            {
+                value = new int[2];
+                value[(int)Attribute.color] = color;
+                value[(int)Attribute.rank] = rank;
+
+                checkColor = false;
+                checkRank = false;
+            }
+        }
+
         static void Main(string[] args)
         {
+            
         }
     }
 }
